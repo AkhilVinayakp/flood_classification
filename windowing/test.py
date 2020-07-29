088c1dccfb3559cf619a1739ae308359fd4ee621
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-07-29 09:32:21 +0530

    testing for new windowed data

---
+++ windowing/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import os
+
+# setting the root directory as the corrent working directory
+os.chdir("/home/po/projects/flood_classification/")
+data = pd.read_csv("windowed.csv")
+b = data[['gx_mean','gx_median','label']]
