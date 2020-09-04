3ed8210d0cee8a9cbd607af3bda8d2bf76761627
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-09-04 20:17:36 +0530

    applyting fft

--- windowing/test.py
+++ windowing/test.py
@@ -6,5 +6,8 @@ import os
 
 # setting the root directory as the corrent working directory
 os.chdir("/home/po/projects/flood_classification/")
+'''
 data = pd.read_csv("windowed.csv")
 b = data[['gx_mean','gx_median','label']]
+'''
+data = pd.read_csv(os.path.join("data","filt_data.csv"))
